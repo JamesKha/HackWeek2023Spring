d8d82a7c93395cae6533e89dbaa1eea40f95ea40
JamesKha <jimmy.kha@mytwu.ca>
2023-03-06 15:26:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,5 @@ import pandas as pd
 
 PAGE_CONFIG = {"page_title":"Hello MLH Global Hack Week","layout":"centered"}
 st.set_page_config(**PAGE_CONFIG)
+st.title('Hello MLH Global Hack Week')
+st.markdown('This website was created by Jimmy (James) Kha')

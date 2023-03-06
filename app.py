b130d3315d8988c6f700956cc6b400e97f53d018
JamesKha <jimmy.kha@mytwu.ca>
2023-03-06 15:30:57 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-PAGE_CONFIG = {"page_title":"Hello MLH Global Hack Week","layout":"centered"}
+PAGE_CONFIG = {"page_title":"Day 1 of Hack Week 2023","layout":"centered"}
 st.set_page_config(**PAGE_CONFIG)
 st.title('Hello MLH Global Hack Week')
 st.markdown('This website was created by Jimmy (James) Kha')
